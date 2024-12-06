3816412ea526afba566ee2f867e43657077c8150
Ramona Bergeron <gberg@bu.edu>
2024-12-06 03:08:44 -0500

    bounding box coordinates correct for most cases

--- cvmodel.py
+++ cvmodel.py
@@ -57,8 +57,8 @@ def process_img(image_path, model):
     scale = 50
 
     
-    new_width = int(width * 50.0/100.0)
-    new_height = int(height * 50.0/100.0)
+    new_width = int(width * scale/100.0)
+    new_height = int(height * scale/100.0)
     image = cv2.resize(image, (new_width, new_height))
 
     original = image.copy()
@@ -96,6 +96,7 @@ def process_img(image_path, model):
     # Find contours, obtain bounding box, extract and save ROI
     ROI_number = 0
     imgs_info = []
+    imgs = []
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     for c in cnts:
@@ -115,20 +116,29 @@ def process_img(image_path, model):
         #print(predicted)
         #print(prediction_label)
 
-        cv2.imwrite('ROI_{}.png'.format(ROI_number), ROI)
+        #cv2.imwrite('ROI_{}.png'.format(ROI_number), ROI)
         ROI_number += 1
+        imgs.append(ROI)
 
         mid_box_coords = [(x + (x+w))/2, (y + (y+h))/2]
+        top_left = [x, y]
 
         #cv2.rectangle(image, (502, 301), (588, 499), (255, 12, 36), 2)
         #cv2.rectangle(image, (1201, 452), (1249, 500), (255, 12, 36), 2)
         
         imgs_info.append({'category': prediction_label, 
-                         'bbox': mid_box_coords
+                         'bbox': top_left
                         })
-        
-    #cv2.imshow('image', image)
-    #cv2.waitKey()
+
+
+    #image = cv2.circle(image, (100,459), radius=6, color=(255, 12, 36), thickness=10)
+    #image = cv2.circle(image, (467,461), radius=6, color=(255, 12, 36), thickness=10)
+    #image = cv2.circle(image, (318, 461), radius=6, color=(255, 12, 36), thickness=10)
+    #image = cv2.circle(image, (368,461), radius=6, color=(255, 12, 36), thickness=10)
+    #image = cv2.circle(image, (301,251), radius=6, color=(255, 12, 36), thickness=10)
+
+    cv2.imshow('image', image)
+    cv2.waitKey()
     #cv2.imshow('gray', gray)
     #cv2.waitKey()
     #cv2.imshow('threshold', thresh)
@@ -136,7 +146,7 @@ def process_img(image_path, model):
 
     #print(imgs_info)
     
-    return image, imgs_info, ROI_number
+    return image, imgs_info, ROI_number, imgs
 
 def detect_object_boundaries(roi):
     """
@@ -194,50 +204,134 @@ def detect_object_boundaries(roi):
     
     return edge_mask
 
-def split_roi(roi, model, label_encoder, rois, j, coords):
+def split_roi(roi, orig_height, orig_width, og_coords):
     """
     Split ROI into sub-regions if multiple objects are detected
     """
     print("split roi")
-    # Detect boundaries
+    print(f"orig height: {orig_height}, orig width: {orig_width}")
     edge_mask = detect_object_boundaries(roi)
     
-    # Find contours in the edge mask
     cnts, _ = cv2.findContours(edge_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    detected=False
     
-    # If no clear separation or too many contours, return the original ROI
     if len(cnts) <= 1:
-        return [roi], rois
+        return [roi], [(0,0)], [og_coords], detected
     
-    # Sort contours by area
+    detected = True
+    
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
     
-    # Store split ROIs
     split_rois = []
-    overlap_factor = 1
+    modified_roi = roi.copy()
 
-    # Extract sub-regions based on contours
+    sub_roi_top_left_coords = []
+
+    sub_roi_coords = []
+
+    global_coords = []
+
     for cnt in cnts:
         x, y, w, h = cv2.boundingRect(cnt)
         
-        # Skip very small regions
         if w < 10 or h < 10:
             continue
         
         sub_roi = roi[y:y+h, x:x+w]    
         if sub_roi.shape == roi.shape:
-            continue        
-
-        x_overlap = int(w * overlap_factor)
-        y_overlap = int(h * overlap_factor)
-
-        x_start = max(x - x_overlap, 0)
-        y_start = max(y - y_overlap, 0)
-        x_end = min(x + w + x_overlap, roi.shape[1])
-        y_end = min(y + h + y_overlap, roi.shape[0])
+            continue
+        
+        x_start = max(int(x - w-4), 0)
+        y_start = max(int(y - h-4), 0)
+        x_end = min(int(x + w + w+4), roi.shape[1])
+        y_end = min(int(y + h + h+4), roi.shape[0])
+        if y_end +10 > orig_height:
+            y_end = orig_height
+        if x_end +10 > orig_width:
+            x_end = orig_width
+        if x_start - 10 < 0:
+            x_start = 0
+        if y_start - 10 < 0:
+            y_start = 0
 
         sub_roi = roi[y_start:y_end, x_start:x_end]    
         split_rois.append(sub_roi)
+
+        #entered in order of 'mid_x', 'mid_y', x_start, x_end, y_start, y_end
+        sub_roi_coords.append([x + w / 2, y + h / 2,x_start,x_end,y_start,y_end])
+        sub_roi_top_left_coords.append((x,y))
+        global_coords.append(og_coords)
+        
+        modified_roi[y_start:y_end, x_start:x_end] =255
+
+    if np.all(np.isin(modified_roi, [255, 220])):
+        return split_rois, sub_roi_top_left_coords, global_coords, detected
+
+    to_remove = set()
+
+    #modified roi coordinates (assume top left corner of roi input is (0,0))
+    m_roi_x = 0
+    m_roi_y = 0
+    for i in range(len(sub_roi_coords)-1):
+        for j in range(i+1,len(sub_roi_coords)):
+            mid_x1, mid_y1, x_start1, x_end1, y_start1, y_end1 = sub_roi_coords[i]
+            mid_x2, mid_y2,x_start2,x_end2,y_start2,y_end2 = sub_roi_coords[j]
+
+            # Horizontal alignment (same y-coordinate)
+            if abs(mid_y1 - mid_y2) <= 3:
+                print("horizontal hit")
+                if x_end1 == orig_width or x_end2 == orig_width: 
+                    if x_start1 == 0 or x_start2 == 0:
+                        if y_start1-10<0:
+                            modified_roi = modified_roi[y_end1:, :] 
+                            m_roi_y = y_end1
+                            to_remove.add(i)
+                            to_remove.add(j)
+                            break
+                        elif y_end1+10>orig_height:
+                            modified_roi = modified_roi[:y_start1, :] 
+                            to_remove.add(i)
+                            to_remove.add(j)
+                            break
+            # Vertical alignment (same x-coordinate)
+            elif abs(mid_x1 - mid_x2) <=3:
+                if y_end1 == orig_height or y_end2 == orig_height: 
+                    if y_start1 == 0 or y_start2 == 0:
+                        if x_start1-10<0:
+                            modified_roi = modified_roi[:, x_end1:] 
+                            m_roi_x = x_end1
+                            to_remove.add(i)
+                            to_remove.add(j)
+                            break
+                        elif x_end +10> orig_width:
+                            modified_roi = modified_roi[:, :x_start1] 
+                            to_remove.add(i)
+                            to_remove.add(j)
+                            break
+    
+    sub_roi_coords = [coords for i, coords in enumerate(sub_roi_coords) if i not in to_remove]    
+    if sub_roi_coords:
+        for i,coords in enumerate(sub_roi_coords):
+            mid_x,mid_y,x_start,x_end,y_start,y_end = coords
+            if y_start > 10:
+                modified_roi = modified_roi[:y_start, :]
+            elif x_end < (orig_width-10):
+                modified_roi = modified_roi[:,x_end:]
+                m_roi_x = x_end
+            elif x_start > 10:
+                modified_roi = modified_roi[:,:x_start]
+            elif y_end < (orig_height-10):
+                modified_roi = modified_roi[y_end:,:]
+                m_roi_y = y_end
+    m_height, m_width = modified_roi.shape[:2]
+    area = m_height * m_width
+    if area > 20:
+        split_rois.append(modified_roi)
+        sub_roi_top_left_coords.append((m_roi_x, m_roi_y))
+        global_coords.append(og_coords)
+    
+    return split_rois, sub_roi_top_left_coords, global_coords, detected
 
     for i, roi in enumerate(split_rois):
         if (i==0):
@@ -246,11 +340,10 @@ def split_roi(roi, model, label_encoder, rois, j, coords):
             roi_filename = "ROI_{}.png".format(i-1+rois)
             
         #print('writing file to ', roi_filename)
+        print(roi)
         cv2.imwrite(roi_filename, roi)
 
-    return split_rois, rois+1
-
-def classify_rois(rois, model, label_encoder, coords):
+def classify_rois(rois, top_left_coords, og_coords, model, label_encoder):
     """
     Classify each split ROI
     """
@@ -258,13 +351,19 @@ def classify_rois(rois, model, label_encoder, coords):
 
     imgs_info = []
     target_size = (32,32)
+
+    #print(len(rois))
+    #print(len(top_left_coords))
     
-    for roi in rois:
+    for i, roi in enumerate(rois):
         # Preprocess ROI for classification
         if roi.shape[0] != target_size[0] or roi.shape[1] != target_size[1]:
             roi_resized = cv2.resize(roi, target_size)
         else:
             roi_resized = roi
+
+        roi_height, roi_width = roi.shape[:2]
+        roi_tensor = preprocess(roi_resized)
 
         roi_tensor = preprocess(roi_resized)
 
@@ -279,42 +378,46 @@ def classify_rois(rois, model, label_encoder, coords):
             if prediction_label == 'banana':
                 banana_height = 35
                 banana_width = 50
-                img = cv2.imread(img_path)
-                height, width,_ = img.shape
-                print(f"{height, width}")
-                if height > banana_height:
+                print(f"{roi_height, roi_width}")
+                if roi_height >= banana_height*2:
                     print("hit")
-                    dup = height / banana_height
-                    for i in range(int(dup)):
-                        sub_roi = roi[banana_height*i:banana_height*(i+1),:]
-                        h, w = sub_roi.shape[:2]
-                        mid_box_coords = [w/2, h/2]
+                    dup = roi_height / banana_height
+                    for h in range(int(dup)):
+                        sub_roi = roi[banana_height*h:banana_height*(h+1),:]
+                        height = banana_height*h
+                        width = 0
+                        mid_box_coords = [height, width]
+                        new_box_coords = [og_coords[i][0]+width, og_coords[i][1]+height]
 
                         # Store object information
                         imgs_info.append({
                             'category': prediction_label, 
-                            'bbox': mid_box_coords,
+                            'bbox': new_box_coords,
                             'roi': sub_roi
                         })
-                if width > banana_width:
+                if roi_width >= banana_width*2:
                     print("hit2")
-                    dup = width / banana_width
+                    dup = roi_width / banana_width
                     print(dup)
-                    for i in range(int(dup)):
-                        sub_roi = roi[:,banana_width*i: banana_width*(i+1)]
-                        h, w = sub_roi.shape[:2]
-                        mid_box_coords = [w/2, h/2]
+                    for j in range(int(dup)):
+                        sub_roi = roi[:,banana_width*j: banana_width*(j+1)]
+                        height = 0
+                        width = banana_width*j
+                        mid_box_coords = [height, width]
+                        new_box_coords = [og_coords[i][0]+width, og_coords[i][1]+height]
 
                         # Store object information
                         imgs_info.append({
                             'category': prediction_label, 
-                            'bbox': mid_box_coords,
+                            'bbox': new_box_coords,
                             'roi': sub_roi
                         })
 
         # Compute midpoint
-        h, w = roi.shape[:2]
-        mid_box_coords = [w/2, h/2]
+        #print('i: ', i)
+        width, height = top_left_coords[i]
+        mid_box_coords = [width+og_coords[i][0], height+og_coords[i][1]]
+        
 
         # Store object information
         imgs_info.append({
@@ -325,48 +428,58 @@ def classify_rois(rois, model, label_encoder, coords):
     
     return imgs_info
 
-def process_roi(img_path, model, label_encoder, rois, j, info, coords):
+def process_roi(roi, model, label_encoder, info, coords):
     """
     Main processing function for a single ROI
     """
     print("process_roi")
     # Read the ROI image
-    roi = cv2.imread(img_path)
+    #roi = cv2.imread(img_path)
+    height, width, _ = roi.shape
+    og_coords = coords
     
     # Split ROI if multiple objects detected
-    split_rois, new_rois = split_roi(roi, model, label_encoder, rois, j, coords)
+    split_rois, top_left_coords, og_coords, detected = split_roi(roi, height, width, og_coords)
+
     
     # Classify split ROIs
-    imgs_info = classify_rois(split_rois, model, label_encoder, coords)
+    imgs_info = classify_rois(split_rois, top_left_coords,og_coords, model, label_encoder)
+
     
-    # Optional: visualize split ROIs
-    for i, info in enumerate(imgs_info):
-        cv2.imshow(f'ROI {i+1} - {info["category"]}', info['roi'])
     
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #Optional: visualize split ROIs
+    #for i, info in enumerate(imgs_info):
+        #print(info)
+        #cv2.imshow(f'ROI {i+1} - {info["category"]}', info['roi'])
     
-    return imgs_info, new_rois
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    
+    return imgs_info
 
 if __name__ == "__main__":
     model = load_model()
-    img_path = 'BTAI_genImages_extra\canvas_0_banana2_monkey1_box4_stack3.png'
+    img_path = 'BTAI_genImages_extra\canvas_0_banana1_monkey1_box4_stack3_banana.png'
     label_encoder = joblib.load('label_encoder.pkl')
-    result_img, info, rois = process_img(img_path, model)
+    result_img, info, rois, imgs = process_img(img_path, model)
 
     new_info=[]
+    remove =[]
+
+    
     
     for i in enumerate(info):
-        img_path = 'ROI_{}.png'.format(i[0])
+        roi = imgs[i[0]]
         #print(img_path)
         
-        results, new_rois = process_roi(img_path, model, label_encoder, rois, i, info, i[1]['bbox'])
+        #print(info[i[0]]['bbox'])
+        results = process_roi(roi, model, label_encoder, info, i[1]['bbox'])
         for k, detection in enumerate(results):
             new_info.append(results[k])
-
+        
     #print(type(info))
     #print(type(new_info))
-
+    
 
     for i, detection in enumerate(new_info):
         print(f"Object {i+1}:")
